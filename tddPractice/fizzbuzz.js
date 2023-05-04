3cd090ee9b3a8caad80dc077a24632e49e516346
Liao Xueqing <xqliao@thoughtworks.com>
2023-05-04 10:24:56 +0800

    Implement for test case: should return FizzBuzz when the number meet the Fizz condition and Buzz condition

--- tddPractice/fizzbuzz.js
+++ tddPractice/fizzbuzz.js
@@ -2,7 +2,14 @@ function isMetFizzCondition(number) {
     return number % 3 === 0 || number.toString().includes('3');
 }
 
+function isMetBuzzCondition(number) {
+    return number % 5 === 0 || number.toString().includes('5');
+}
+
 function fizzBuzz(number) {
+    if (isMetFizzCondition(number) && isMetBuzzCondition(number)) {
+        return 'FizzBuzz'
+    }
     if (isMetFizzCondition(number)) {
         return 'Fizz'
     }
