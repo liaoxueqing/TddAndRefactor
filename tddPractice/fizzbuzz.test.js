fff72bfd0ca3f494f1bf90272065abb52f8bc849
Liao Xueqing <xqliao@thoughtworks.com>
2023-05-04 10:23:33 +0800

    Add test case: should return FizzBuzz when the number meet the Fizz condition and Buzz condition

--- tddPractice/fizzbuzz.test.js
+++ tddPractice/fizzbuzz.test.js
@@ -21,4 +21,24 @@ describe('fizzBuzz', () => {
         const actual = fizzBuzz(25)
         expect(actual).toBe('Buzz')
     })
+
+    it('should return FizzBuzz when the number is multiple 3 and 5', () => {
+        const actual = fizzBuzz(15)
+        expect(actual).toBe('FizzBuzz')
+    })
+
+    it('should return FizzBuzz when the number is multiple 5 and includes 3', () => {
+        const actual = fizzBuzz(35)
+        expect(actual).toBe('FizzBuzz')
+    })
+
+    it('should return FizzBuzz when the number is multiple 3 and includes 5', () => {
+        const actual = fizzBuzz(54)
+        expect(actual).toBe('FizzBuzz')
+    })
+
+    it('should return FizzBuzz when the number is includes 3 and 5', () => {
+        const actual = fizzBuzz(53)
+        expect(actual).toBe('FizzBuzz')
+    })
 })
